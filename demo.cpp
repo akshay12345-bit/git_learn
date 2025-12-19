a1c75bb7b29b2026b3b0205726cebd6ce7aa2d35
akshay201104 <akshaytqpwo@gmail.com>
2025-12-19 21:39:34 +0530

    check even odd

--- demo.cpp
+++ demo.cpp
@@ -3,8 +3,24 @@
 
 # include <iostream>  //correction
 using namespace std;
+
+//as per the request i am writing a program on checking weather a number is odd or even
+
+void check_odd_even(int x){
+  if(x%2==0){
+    cout<<"even number"<<endl;
+  }
+  else{
+    cout<<"odd number"<<endl;
+  }
+}
+
 int main(){
-  cout<<"hello..."<<endl;
-  cout<<"i am learning python"<<endl;
+  // cout<<"hello..."<<endl;
+  // cout<<"i am learning python"<<endl;
+
+  //calling the function
+  check_odd_even(56);
+
   return 0;
 }
